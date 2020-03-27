5456fbb9325082b1a68444a275d6f00634367a82
Lexa070301 <Lexa070301@mail.ru>
2020-03-27 20:23:54 +0300

    create header block

---
+++ 7laba/js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    $("input[type='tel']").mask("+7(999)999-99-99");
+
+    $('.header__arrow, .header__menu__item__link').on( 'click', function(){
+        var el = $(this);
+        var dest = el.attr('href'); // получаем направление
+        if(dest !== undefined && dest !== '') { // проверяем существование
+            $('html').animate({
+                    scrollTop: $(dest).offset().top // прокручиваем страницу к требуемому элементу
+                }, 1000 // скорость прокрутки
+            );
+        }
+        return false;
+    });
+});
